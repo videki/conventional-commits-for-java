ac9dc9ac8faa3877317d6d31215531b2102538bb
videki <levente.ban@gmail.com>
2022-07-06 18:43:05 +0200

    docs: javadoc enhancements.

--- conventional-commits-common/src/main/java/net/videki/semver/cc/release/common/CommitAdapter.java
+++ conventional-commits-common/src/main/java/net/videki/semver/cc/release/common/CommitAdapter.java
@@ -1,5 +1,10 @@
 package net.videki.semver.cc.release.common;
 
+/**
+ * SCM commit msg abstraction.
+ *
+ * @param <T> The scm-specific commit.
+ */
 public interface CommitAdapter<T>
 {
     String getShortMessage();
